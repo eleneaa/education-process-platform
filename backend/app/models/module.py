a596081722f53ec3df94af77de81052b12ecdb00
eleneaa <lenatisenko4@gmail.com>
2026-03-15 00:47:37 +0500

    [ADD] add module model

--- backend/app/models/module.py
+++ backend/app/models/module.py
@@ -3,55 +3,50 @@ from datetime import datetime
 
 from sqlmodel import SQLModel, Field, Relationship
 
-from . import User
-from .enums import ProgramStatus
+from .program import Program
 from .utils import get_datetime_utc
 
 
-class ProgramBase(SQLModel):
+class ModuleBase(SQLModel):
     title: str = Field(min_length=1, max_length=255)
     description: str | None = Field(default=None, max_length=255)
+    position: int | None = None
 
 
-class ProgramCreate(ProgramBase):
-    pass
+class ModuleCreate(ModuleBase):
+    program_id: uuid.UUID
 
 
-class ProgramUpdate(SQLModel):
+class ModuleUpdate(SQLModel):
     title: str | None = Field(default=None, min_length=1, max_length=255)
     description: str | None = Field(default=None, max_length=255)
-    status: ProgramStatus | None = None
+    position: int | None = None
 
 
-class ProgramPublic(ProgramBase):
+class ModulePublic(ModuleBase):
     id: uuid.UUID
     created_at: datetime | None = None
     updated_at: datetime | None = None
-    status: ProgramStatus = Field(default=ProgramStatus.DRAFT)
-    created_by_id: uuid.UUID | None = None
+    program_id: uuid.UUID
 
 
-class ProgramsPublic(SQLModel):
-    data: list[ProgramPublic]
+class ModulesPublic(SQLModel):
+    data: list[ModulePublic]
     count: int
 
-class Program (ProgramBase, table=True):
+class Module (ModuleBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     created_at: datetime | None = Field(
         default_factory=get_datetime_utc,
         sa_type=DateTime(timezone=True),  # type: ignore
     )
     updated_at: datetime | None = None
-    status: ProgramStatus = Field(default=ProgramStatus.DRAFT)
-    created_by_id: uuid.UUID | None = Field(
+    program_id: uuid.UUID | None = Field(
         default=None,
-        foreign_key="user.id",
+        foreign_key="program.id",
         nullable=True,
-        ondelete="SET NULL",
+        ondelete="CASCADE",
     )
-    created_by: User | None = Relationship(
-        back_populates="created_programs",
-    )
-    modules: list["Module"] = Relationship(
-        back_populates="program"
+    program: Program | None = Relationship(
+        back_populates="modules",
     )
